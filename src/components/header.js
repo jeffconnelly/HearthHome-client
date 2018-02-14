10571ed29868d6c7b52a43b455df13313a0875fd
Jeff Connelly <jeff.connelly6@gmail.com>
2018-02-14 11:27:54 -0700

    Added header styling

--- src/components/header.js
+++ src/components/header.js
@@ -4,8 +4,10 @@ import './header.css';
 export default function Header(props) {
   return (
     <div className="header">
+      <div className="header-wrapper">
       <h1 className="header-text">HearthHome</h1>
       <h2 className="header-sub">Your Hearthstone Card Database and Deck Builder Site</h2>
+      </div>
     </div>
   )
 }
