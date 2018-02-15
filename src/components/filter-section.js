9cb0e03cbb45b6cc1a7015a35dfadaa243c15aae
Jeff Connelly <jeff.connelly6@gmail.com>
2018-02-15 12:50:17 -0700

    Can render variable images in class section

--- src/components/filter-section.js
+++ src/components/filter-section.js
@@ -107,12 +107,12 @@ export class FilterSection extends React.Component {
       <img onClick={() => this.props.dispatch(fetchClassNeutralCards('Mage', 'Mage'))} className="hero-img" src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/3/3c/Jaina_Proudmoore%28320%29.png" alt="Jaina Proudmoore"/>
       <img onClick={() => this.props.dispatch(fetchClassNeutralCards('Druid', 'Druid'))} className="hero-img" src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/f/fa/Malfurion_Stormrage%28621%29.png?version=19f80aa69e2bd7caa7fe66519a56a3be"  alt="Malfurion Stormrage"/>
       <img onClick={() => this.props.dispatch(fetchClassNeutralCards('Hunter', 'Hunter'))} className="hero-img" src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/a/a0/Rexxar%28484%29.png?version=02018dce868e603f5495b4dff74dcbf7"  alt="Rexxar"/>
-      <img onClick={() => this.props.dispatch(fetchClassNeutralCards('Paladin', 'Paladin'))} className="hero-img"src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/thumb/4/4d/Uther_Lightbringer%28257%29.png/250px-Uther_Lightbringer%28257%29.png?version=fea64b3dc715b39fbff79d20346ced38"  alt="Uther Lightbringer"/>
-      <img onClick={() => this.props.dispatch(fetchClassNeutralCards('Priest', 'Priest'))}className="hero-img"src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/thumb/8/80/Anduin_Wrynn%28110%29.png/250px-Anduin_Wrynn%28110%29.png?version=7a5312f9023ca42a6f08ac4117b3432d"  alt="Anduin Wrynn"/>
-      <img onClick={() => this.props.dispatch(fetchClassNeutralCards('Rogue', 'Rogue'))} className="hero-img"src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/thumb/a/a4/Valeera_Sanguinar%282%29.png/250px-Valeera_Sanguinar%282%29.png?version=35a50a374b283d740a7aab740f5f6377"  alt="Valeera Sanguinar"/>
-      <img onClick={() => this.props.dispatch(fetchClassNeutralCards('Shaman', 'Shaman'))} className="hero-img"src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/thumb/4/4b/Thrall%28319%29.png/250px-Thrall%28319%29.png?version=e99c70006f2da990c432fe9a55fc5be1"  alt="Thrall"/>
-      <img onClick={() => this.props.dispatch(fetchClassNeutralCards('Warlock', 'Warlock'))} className="hero-img"src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/thumb/0/0a/Gul%27dan%28618%29.png/250px-Gul%27dan%28618%29.png?version=d54808871d68a28cb8b647b6e7f7be90"  alt="Gul'dan"/>
-      <img onClick={() => this.props.dispatch(fetchClassNeutralCards('Warrior', 'Warrior'))}  className="hero-img"src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/thumb/c/c5/Garrosh_Hellscream%28635%29.png/250px-Garrosh_Hellscream%28635%29.png?version=b06686c37d1aafb707b9b4b6e924bc57" alt="Garrosh Hellscream"/>
+      <img onClick={() => this.props.dispatch(fetchClassNeutralCards('Paladin', 'Paladin'))} className="hero-img" src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/thumb/4/4d/Uther_Lightbringer%28257%29.png/250px-Uther_Lightbringer%28257%29.png?version=fea64b3dc715b39fbff79d20346ced38"  alt="Uther Lightbringer"/>
+      <img onClick={() => this.props.dispatch(fetchClassNeutralCards('Priest', 'Priest'))} className="hero-img" src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/thumb/8/80/Anduin_Wrynn%28110%29.png/250px-Anduin_Wrynn%28110%29.png?version=7a5312f9023ca42a6f08ac4117b3432d"  alt="Anduin Wrynn"/>
+      <img onClick={() => this.props.dispatch(fetchClassNeutralCards('Rogue', 'Rogue'))} className="hero-img" src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/thumb/a/a4/Valeera_Sanguinar%282%29.png/250px-Valeera_Sanguinar%282%29.png?version=35a50a374b283d740a7aab740f5f6377"  alt="Valeera Sanguinar"/>
+      <img onClick={() => this.props.dispatch(fetchClassNeutralCards('Shaman', 'Shaman'))} className="hero-img" src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/thumb/4/4b/Thrall%28319%29.png/250px-Thrall%28319%29.png?version=e99c70006f2da990c432fe9a55fc5be1"  alt="Thrall"/>
+      <img onClick={() => this.props.dispatch(fetchClassNeutralCards('Warlock', 'Warlock'))} className="hero-img" src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/thumb/0/0a/Gul%27dan%28618%29.png/250px-Gul%27dan%28618%29.png?version=d54808871d68a28cb8b647b6e7f7be90"  alt="Gul'dan"/>
+      <img onClick={() => this.props.dispatch(fetchClassNeutralCards('Warrior', 'Warrior'))}  className="hero-img" src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/thumb/c/c5/Garrosh_Hellscream%28635%29.png/250px-Garrosh_Hellscream%28635%29.png?version=b06686c37d1aafb707b9b4b6e924bc57" alt="Garrosh Hellscream"/>
       </div>
       </section>
     );
@@ -124,6 +124,8 @@ export class FilterSection extends React.Component {
       <h2>Build Your {this.props.Class} Deck</h2>
       <h3>Click on the cards to add to your deck</h3>
       <div className="db-mode-wrapper">
+      <img src={`http://www.deckselect.eu/img/${this.props.Class}.png`} />
+      <img src={`http://metastats.net/images/class/neutral.png`} />
       <button onClick={() => this.props.dispatch(leaveDbMode())}
       >
       Leave Deck Builder Mode
@@ -146,3 +148,4 @@ export default connect (mapStateToProps)(FilterSection);
 
 
 // disabled hidden
+
